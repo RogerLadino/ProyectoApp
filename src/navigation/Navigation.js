1d186ac2b0c3d3f189b38cf197e8f0ba2b9e0815
NICOLAS2006-APRENDIZ <nicolasvanegas171@gmail.com>
2025-11-29 12:52:49 -0500

    feat: add classroom routes to navigation stack

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -2,14 +2,42 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeView from '../views/HomeView';
+import ListClassroomView from '../views/Classroom/ListClassroomView';
+import CreateClassroomView from '../views/Classroom/CreateClassroomView';
+import EditClassroomView from '../views/Classroom/EditClassroomView';
 
 const Stack = createNativeStackNavigator();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeView} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: '#231F20' },
+          headerTintColor: '#FBFBFB',
+          headerTitleStyle: { fontWeight: 'bold' },
+        }}
+      >
+        <Stack.Screen 
+          name="Home" 
+          component={HomeView} 
+          options={{ title: 'Inicio' }}
+        />
+        <Stack.Screen 
+          name="ListClassroom" 
+          component={ListClassroomView} 
+          options={{ title: 'Mis Clases' }}
+        />
+        <Stack.Screen 
+          name="CreateClassroom" 
+          component={CreateClassroomView} 
+          options={{ title: 'Crear Clase' }}
+        />
+        <Stack.Screen 
+          name="EditClassroom" 
+          component={EditClassroomView} 
+          options={{ title: 'Editar Clase' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
