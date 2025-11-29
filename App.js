29e560c809ed8a743c51a5dc56cb92ce76c60c15
NICOLAS2006-APRENDIZ <nicolasvanegas171@gmail.com>
2025-11-29 12:53:42 -0500

    feat: integrate ClassroomProvider in App.js

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
+
 import { StatusBar } from 'expo-status-bar';
 import Navigation from './src/navigation/Navigation';
+import ClassroomProvider from './src/context/ClassroomProvider';
+import { AuthProvider } from './src/context/AuthContext'; // Si tienes autenticación
 
 export default function App() {
   return (
-    <>
-      <Navigation />
-      <StatusBar style="auto" />
-    </>
+    <AuthProvider>
+      <ClassroomProvider>
+        <Navigation />
+        <StatusBar style="auto" />
+      </ClassroomProvider>
+    </AuthProvider>
   );
 }
