2a5284f3462ce0af127be11f27a551499ea9a5f3
RogerLadino <ztroger15@gmail.com>
2025-12-10 15:59:56 -0500

    fix: Quitar importación no usada de View

--- src/components/Classroom/ActionButton.js
+++ src/components/Classroom/ActionButton.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { colors, spacing, borderRadius } from '../../constant/theme';
 
