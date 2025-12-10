070154ece11629b4aa822ed3cdd1e49a556ca2ae
RogerLadino <ztroger15@gmail.com>
2025-12-10 17:10:09 -0500

    fix: Quitar importación no usada de View

--- src/components/Code/UserList.js
+++ src/components/Code/UserList.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { View, Text, ScrollView, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, ScrollView, TouchableOpacity, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 const UserList = ({ submissions, currentUserId, onSelectUser }) => {
