98f8a8685fe25a3dbd0b68771717b94f61edc763
NICOLAS2006-APRENDIZ <nicolasvanegas171@gmail.com>
2025-12-11 17:43:43 -0500

    fix: remove unused route and showWarning variables in ExerciseProfessorView

--- src/views/Exercise/ExerciseProfessorView.js
+++ src/views/Exercise/ExerciseProfessorView.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { View, ScrollView, StyleSheet } from 'react-native';
-import { useNavigation, useRoute } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import { useExercise } from '../../context/Exercise';
 import { useCode } from '../../context/Code';
-import { useNotification } from '../../context/NotificationContext';
 import { ClassroomContext } from '../../context/ClassroomProvider';
 import LoadingScreen from '../../components/Common/LoadingScreen';
 import ProfessorHeader from '../../components/Layout/ProfessorHeader';
@@ -14,11 +13,9 @@ import { colors, spacing } from '../../constant/theme';
 
 const ExerciseProfessorView = () => {
   const navigation = useNavigation();
-  const route = useRoute();
   const { currentClassroomId } = useContext(ClassroomContext);
   const { currentExercise, currentExerciseId, loading: exerciseLoading, fetchExerciseById } = useExercise();
   const { submissions, loading: submissionsLoading, fetchSubmissions, updateGrade, setCurrentUserId } = useCode();
-  const { showWarning } = useNotification();
   const [loading, setLoading] = useState(true);
   const [localSubmissions, setLocalSubmissions] = useState([]);
 
